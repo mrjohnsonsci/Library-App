cbf7c1d28bf4f9941da9dfe3e4ac982f792f9ac8
Peter Johnson <pjohnson141@gmail.com>
2017-07-19 03:53:14 +0000

    little changes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
   get 'programs/climate'
   get 'programs/future'
 
-
   resources :requests
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
